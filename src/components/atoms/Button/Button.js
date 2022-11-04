3f3ebed99ece14283ea22092704130004a4436c0
Revku <office@revku.dev>
2022-11-04 21:48:36 +0100

    Add button component

---
+++ src/components/atoms/Button/Button.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import * as styles from './Button.module.scss';
+
+const Button = ({ action, children }) => {
+  return (
+    <button className={styles.button} onClick={action} type="button">
+      {children}
+    </button>
+  );
+};
+
+Button.propTypes = {
+  action: PropTypes.func,
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]).isRequired,
+};
+
+Button.defaultProps = {
+  action: () => false,
+};
+
+export default Button;
