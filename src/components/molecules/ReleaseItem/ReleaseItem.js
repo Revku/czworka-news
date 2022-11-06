ee42c87423de75c810d9b6a5445de8448b039bca
Revku <office@revku.dev>
2022-11-06 15:26:06 +0100

    Add hover animation to Release Item

--- src/components/molecules/ReleaseItem/ReleaseItem.js
+++ src/components/molecules/ReleaseItem/ReleaseItem.js
@@ -1,15 +1,22 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 import PropTypes from 'prop-types';
 import * as styles from './ReleaseItem.module.scss';
 
 const ReleaseItem = ({ year, name, color, url }) => {
   return (
-    <a href={url} target="_blank" rel="noreferrer" className={styles.wrapper}>
+    <motion.a
+      whileHover={{ y: -5 }}
+      href={url}
+      target="_blank"
+      rel="noreferrer"
+      className={styles.wrapper}
+    >
       <p className={styles.year} style={{ backgroundColor: color }}>
         {year}
       </p>
       <p className={styles.name}>{name}</p>
-    </a>
+    </motion.a>
   );
 };
 
