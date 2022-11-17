f466f2b5b60a8e2eb0db36a339707ccb8ecd50ce
Revku <office@revku.dev>
2022-11-17 19:08:46 +0100

    Add 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import * as styles from 'styles/custom.module.scss';
+import SEO from 'components/SEO';
+import logo from 'images/logo.svg';
+import Button from 'components/atoms/Button/Button';
+import { Link } from 'gatsby';
 
 const NotFoundPage = () => {
-  return <div>NotFoundPage</div>;
+  return (
+    <div className={styles.wrapper}>
+      <img src={logo} alt="Czwórka News Logo" />
+      <h2 className={styles.title}>Nie znaleziono!</h2>
+      <p className={styles.paragraph}>
+        Strona, której szukasz nie istnieje lub została usunięta. Przepraszamy
+        za utrudnienia.
+      </p>
+      <Link to="/" className={styles.button}>
+        <Button>Powrót do strony głównej</Button>
+      </Link>
+    </div>
+  );
 };
 
 export default NotFoundPage;
+export const Head = () => <SEO title="Wystąpił błąd! - Czwórka News" />;
