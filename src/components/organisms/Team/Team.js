dd3ffb67bd8ecda2cc01caaea469ed440c6cd464
Revku <office@revku.dev>
2022-11-20 10:35:12 +0100

    Add sections scroll animation

--- src/components/organisms/Team/Team.js
+++ src/components/organisms/Team/Team.js
@@ -8,30 +8,32 @@ const Team = () => {
 
   return (
     <div className={styles.wrapper}>
-      <h2 className={styles.title}>
-        Nasz <span className={styles.mark}>zespół</span>
-      </h2>
-      <p className={styles.paragraph}>
-        Nasz zespół od Września 2017 roku pracuje nad najnowszymi numerami
-        gazetki. Nasi Redaktorzy starają się robić: profesjonalne researche,
-        ładnie wydrukowane numery oraz starają się wprowadzać nowości dla
-        naszych czytelników.
-      </p>
+      <div id="teamContent" className={styles.teamContent}>
+        <h2 className={styles.title}>
+          Nasz <span className={styles.mark}>zespół</span>
+        </h2>
+        <p className={styles.paragraph}>
+          Nasz zespół od Września 2017 roku pracuje nad najnowszymi numerami
+          gazetki. Nasi Redaktorzy starają się robić: profesjonalne researche,
+          ładnie wydrukowane numery oraz starają się wprowadzać nowości dla
+          naszych czytelników.
+        </p>
 
-      <div className={styles.content}>
-        {data.map((item) => {
-          return (
-            <Redactor
-              key={item.id}
-              firstName={item.firstName}
-              lastName={item.lastName}
-              roleColor={item.roleColor.hex}
-              roleName={item.role}
-              about={item.about}
-              photo={item.photo.url}
-            />
-          );
-        })}
+        <div className={styles.content}>
+          {data.map((item) => {
+            return (
+              <Redactor
+                key={item.id}
+                firstName={item.firstName}
+                lastName={item.lastName}
+                roleColor={item.roleColor.hex}
+                roleName={item.role}
+                about={item.about}
+                photo={item.photo.url}
+              />
+            );
+          })}
+        </div>
       </div>
     </div>
   );
